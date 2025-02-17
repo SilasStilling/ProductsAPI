e42b8d4ce1b9bda01b678d989bc218c4a311fccf
Silas Stilling-Jørgensen <sis002@edu.zealand.dk>
2025-02-17 15:20:58 +0100

    ny

--- ProductsAPI/Controllers/ProductsController.cs
+++ ProductsAPI/Controllers/ProductsController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebShopLibrary;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace ProductsAPI.Controllers
 {
     [Route("api/[controller]")]
